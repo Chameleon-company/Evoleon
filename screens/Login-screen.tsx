2c4a54c94507f745474ae77c98e0129d4fc7121b
abethedev <94020440+abethedev@users.noreply.github.com>
2022-09-06 23:36:34 +1000

    Update Login-screen.tsx
    changed login-screen to a class (might change back)

--- screens/Login-screen.tsx
+++ screens/Login-screen.tsx
@@ -11,56 +11,58 @@ import { useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { LoginStyle } from "../styles/login";
 
-export default function LoginScreen() {
-  const navigation = useNavigation();
+export default class LoginScreen extends React.Component {
+  render() {
+    // const navigation = useNavigation();
 
-  useEffect(() => {
-    navigation.setOptions({
-      headerLeft: (props: StackHeaderLeftButtonProps) => <MenuIcon />,
-    });
-  });
+    // useEffect(() => {
+    //   navigation.setOptions({
+    //     headerLeft: (props: StackHeaderLeftButtonProps) => <MenuIcon />,
+    //   });
+    // });
 
-  return (
-    <SafeAreaView style={LoginStyle.content}>
-      <View style={LoginStyle.upperContent}>
-        <Text>Don't have an account?</Text>
-        <Pressable style={LoginStyle.buttonTextOnly}>
-          <Text style={LoginStyle.SignupLink}>Sign Up</Text>
-        </Pressable>
-      </View>
-      <View style={LoginStyle.inputView}>
-        <TextInput
-          style={LoginStyle.input}
-          label="Email Address"
-          keyboardType="email-address"
-        ></TextInput>
-        <TextInput
-          style={LoginStyle.input}
-          label="Password"
-          secureTextEntry={true}
-        ></TextInput>
+    return (
+      <SafeAreaView style={LoginStyle.content}>
+        <View style={LoginStyle.upperContent}>
+          <Text>Don't have an account?</Text>
+          <Pressable style={LoginStyle.buttonTextOnly}>
+            <Text style={LoginStyle.SignupLink}>Sign Up</Text>
+          </Pressable>
+        </View>
+        <View style={LoginStyle.inputView}>
+          <TextInput
+            style={LoginStyle.input}
+            label="Email Address"
+            keyboardType="email-address"
+          ></TextInput>
+          <TextInput
+            style={LoginStyle.input}
+            label="Password"
+            secureTextEntry={true}
+          ></TextInput>
 
-        <Pressable style={LoginStyle.forgotPassButton}>
-          <Text style={LoginStyle.SignupLink}>Forgot your password?</Text>
-        </Pressable>
+          <Pressable style={LoginStyle.forgotPassButton}>
+            <Text style={LoginStyle.SignupLink}>Forgot your password?</Text>
+          </Pressable>
 
-        {/* Sign in button */}
-        <Pressable
-          style={LoginStyle.button}
-          onPress={() => {
-            console.log("Pressed sign up button");
+          {/* Sign in button */}
+          <Pressable
+            style={LoginStyle.button}
+            onPress={() => {
+              console.log("Pressed sign up button");
 
-            // Navigation link to be improved
-            navigation.navigate("DatabaseScreen");
-          }}
-        >
-          <Text style={buttonStyles.Text}>Sign In</Text>
-        </Pressable>
+              // Navigation link to be improved
+              navigation.navigate("DatabaseScreen");
+            }}
+          >
+            <Text style={buttonStyles.Text}>Sign In</Text>
+          </Pressable>
 
-        <Pressable style={LoginStyle.cancelButton}>
-          <Text style={LoginStyle.cancelText}>Cancel</Text>
-        </Pressable>
-      </View>
-    </SafeAreaView>
-  );
+          <Pressable style={LoginStyle.cancelButton}>
+            <Text style={LoginStyle.cancelText}>Cancel</Text>
+          </Pressable>
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
