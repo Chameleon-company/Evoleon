8449c60e57a779a8a1439097fd45c075c3dc71b7
Leda <48677818+Leda-s@users.noreply.github.com>
2022-09-19 11:41:34 +1000

    Revert "Attempt at Google sign in"
    This reverts commit 33f4d5fb6502cce5d0334189e86972854581f135.

--- screens/Login-screen.tsx
+++ screens/Login-screen.tsx
@@ -9,15 +9,15 @@ import MenuIcon from "../components/MenuIcon";
 import { useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { LoginStyle } from "../styles/login";
-import { userSignIn } from "../web/firebase";
+import {userSignIn} from '../web/firebase'
 
-{
-  /* Log in Screen */
-}
+
+{/* Log in Screen */}
 export default function LoginScreen() {
   const navigation = useNavigation();
   const [email, onChangeTextEmail] = React.useState("");
   const [password, onChangeTextPassword] = React.useState("");
+
 
   useEffect(() => {
     navigation.setOptions({
@@ -25,34 +25,21 @@ export default function LoginScreen() {
     });
   });
 
+
   return (
     <SafeAreaView style={LoginStyle.content}>
       <View style={LoginStyle.upperContent}>
         <Text>Don't have an account?</Text>
-        <Pressable
-          onPress={() => {
-            navigation.navigate("Database");
-          }}
-        >
+        <Pressable style={LoginStyle.buttonTextOnly} onPress={() => {
+            navigation.navigate("Database")
+          }}>
           <Text style={LoginStyle.SignupLink}>Sign Up</Text>
         </Pressable>
       </View>
 
       <View style={LoginStyle.inputView}>
-        <TextInput
-          style={LoginStyle.input}
-          keyboardType="email-address"
-          onChangeText={onChangeTextEmail}
-          value={email}
-          placeholder="Email"
-        />
-        <TextInput
-          style={LoginStyle.input}
-          secureTextEntry={true}
-          onChangeText={onChangeTextPassword}
-          value={password}
-          placeholder="Password"
-        />
+        <TextInput style={LoginStyle.input} keyboardType="email-address" onChangeText={onChangeTextEmail} value={email} placeholder="Email"/>
+        <TextInput style={LoginStyle.input} secureTextEntry={true} onChangeText={onChangeTextPassword} value={password} placeholder="Password"/>
 
         <Pressable style={LoginStyle.forgotPassButton}>
           <Text style={LoginStyle.SignupLink}>Forgot your password?</Text>
@@ -61,11 +48,10 @@ export default function LoginScreen() {
         {/* Sign in button */}
         <Pressable
           style={LoginStyle.button}
-          onPress={() => {
-            userSignIn(email, password);
-            navigation.navigate("Database");
-          }}
-        >
+            onPress={() => {
+              userSignIn(email, password);
+              navigation.navigate("Database")
+          }}>
           <Text style={buttonStyles.Text}>Sign In</Text>
         </Pressable>
 
