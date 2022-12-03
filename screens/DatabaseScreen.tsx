4c28addf43a05a0970c975f3af9a0413ede10997
Leda <48677818+Leda-s@users.noreply.github.com>
2022-12-03 18:58:48 +1100

    Remove commented code from T2

--- screens/DatabaseScreen.tsx
+++ screens/DatabaseScreen.tsx
@@ -58,65 +58,6 @@ export default function DatabaseScreen() {
     })();
   }, []);
 
-  //   return (
-  //     <View style={styles.container}>
-  //       <MapView
-  //         style={{ alignSelf: 'stretch', height: '100%' }}
-  //         // region={mapRegion}
-  //        initialRegion={{
-  //           latitude: -37.840935,
-  //           longitude: 144.946457,
-  //           latitudeDelta: 0.922,
-  //           longitudeDelta: 0.0421,
-  //         }}
-  //         >
-
-  //         <Marker 
-  //         coordinate={{latitude:-37.875937,
-  //         longitude:145.159616,}}
-  //         > 
-  //          <Image source={require('../assets/EvoleonFinal.png')} style={{height: 20, width:20 }} />
-
-  //          </Marker>
-  //          <Marker 
-  //         coordinate={{latitude:-37.804359,
-  //         longitude:144.87952,}}
-  //         > 
-  //          <Image source={require('../assets/EvoleonFinal.png')} style={{height: 20, width:20 }} />
-
-  //          </Marker>
-  //          <Marker 
-  //         coordinate={{latitude:-37.738141,
-  //         longitude:144.971459,}}
-  //         > 
-  //          <Image source={require('../assets/EvoleonFinal.png')} style={{height: 20, width:20 }} />
-
-  //          </Marker>
-  //          <Marker 
-  //         coordinate={{latitude:-37.897614,
-  //         longitude:145.015414,}}
-  //         > 
-  //          <Image source={require('../assets/EvoleonFinal.png')} style={{height: 20, width:20 }} />
-
-  //          </Marker>
-  //       </MapView>
-
-  //     </View>
-  //   );
-  // };
-
-  // const styles = StyleSheet.create({
-  //   container: {
-  //     flex: 1,
-  //     backgroundColor: '#fff',
-  //     alignItems: 'center',
-  //     justifyContent: 'center',
-  //   },
-  //   map: {
-  //     width: Dimensions.get('window').width,
-  //     height: Dimensions.get('window').height,
-  //   },
-  // });
 
   const markers = [
     {
