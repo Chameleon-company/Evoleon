8ffe826e3a24d388a875b778a9320c55676e615f
X0RA <annand.rodney@gmail.com>
2023-03-26 23:57:25 +1100

    some comments on the issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
-module.exports = async function (env, argv) {
-  var config = await createExpoWebpackConfigAsync(env, argv);
-  
-  config.resolve.alias['react-native'] = 'react-native-web';
-  config.resolve.alias['react-native-maps'] = 'react-native-web-maps';
+module.exports = async function(env, argv) {
+	var config = await createExpoWebpackConfigAsync(env, argv);
 
-  // Customize the config before returning it.
-  return config;
+	// These once worked, now they do not. The webview is broken but if you comment out the "MapView" in "databasescreeen" you can get it working.
+	// The problem is that webpack, and the react-native / react-native maps are not playing nice together.
+
+	// config.resolve.alias['react-native'] = 'react-native-web';
+	// config.resolve.alias['react-native-maps'] = 'react-native-web-maps';
+
+	// Customize the config before returning it.
+	return config;
 };
