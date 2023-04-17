2bba1381617ca1b81ec8dfeefcbc158731d64552
M1ck0 <m1ck0@pm.me>
2023-04-17 18:16:41 +1000

    Adds App Version & Update Button
    This commit adds the current app version and the "Update" app button. The app button does not currently do anything other than fade when pressed.

--- screens/AboutScreen.tsx
+++ screens/AboutScreen.tsx
@@ -1,14 +1,23 @@
-import * as React from "react";
-import { useNavigation } from "@react-navigation/native";
-import { StackHeaderLeftButtonProps } from "@react-navigation/stack";
+import * as React from 'react';
+import { StyleSheet, Button, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { StackHeaderLeftButtonProps } from '@react-navigation/stack';
+//import DeviceInfo from 'react-native-device-info';
 
 import { Text, View } from "../components/Themed";
 import MenuIcon from "../components/MenuIcon";
 import { useEffect } from "react";
 import main from "../styles/main";
+import { ButtonStyle } from '../styles/buttonStyle';
 
-export default function AboutScreen() {
+function AboutScreen() {
     const navigation = useNavigation();
+    const EvoleonAppDeets = require('../app.json');
+    const deviceAppVersion = 'Pickles' //DeviceInfo.getVersion();
+    const upDateButton = 'Update';
+
+
+    console.log(EvoleonAppDeets.expo.version);
     
     useEffect(() => {
         navigation.setOptions({
@@ -18,10 +27,33 @@ export default function AboutScreen() {
 
     return (
     <View style={main.centered}>
-        <Text lightColor="rgba(0,0,0,0.8)" darkColor="rgba(255,255,255,0.8)">
-        This is the About Screen! 
-        </Text>
+        <Text style={styles.currentVersion} lightColor="rgba(0,0,0,0.8)" darkColor="rgba(255,255,255,0.8)">
+        App Version: {deviceAppVersion} </Text>
+        <Text style={styles.updateVersion} lightColor="rgba(0,0,0,0.8)" darkColor="rgba(255,255,255,0.8)">
+        Version Available: {EvoleonAppDeets.expo.version} </Text>
+        <View style={styles.updateButton}>
+          <TouchableOpacity style={ButtonStyle.Button}>
+            <Text style={ButtonStyle.Text}>{upDateButton}</Text>
+          </TouchableOpacity>
+        </View>
     </View>
     );
 
 }
+
+export const styles = StyleSheet.create({
+  currentVersion: {
+    position: 'absolute',
+    top: 100,
+  },
+  updateButton: {
+    position: 'absolute',
+    top: 160,
+  },
+  updateVersion: {
+    position: 'absolute',
+    top: 120,
+  },
+})
+
+export default AboutScreen;
--- styles/buttonStyle.ts
+++ styles/buttonStyle.ts
@@ -1,14 +1,19 @@
 import { StyleSheet } from 'react-native';
 
-export const ButtonStyle = StyleSheet.create({
+export const ButtonStyle = StyleSheet.create({  // export const ButtonStyle = StyleSheet.create({ // Replaced with export default. 
   Button: {
     alignItems: 'center',
     justifyContent: 'center',
-    paddingVertical: 12,
-    paddingHorizontal: 120,
+    paddingVertical: 8,
+    paddingHorizontal: 90,
     borderRadius: 50,
     backgroundColor: '#294E4B',
     marginBottom: 40,
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
   },
   
   Text: {
