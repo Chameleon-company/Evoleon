615a55361cd68f7d0b1c8d0f5af70a116ebf6ced
Ninny659 <jordansamcooke@gmail.com>
2023-04-20 19:18:10 +1000

    Adds button to profile tab - functionality.

--- screens/ClientsScreen.tsx
+++ screens/ClientsScreen.tsx
@@ -2,7 +2,7 @@
 import * as React from "react";
 import { useNavigation } from "@react-navigation/native";
 import { StackHeaderLeftButtonProps } from "@react-navigation/stack";
-import { Image, TouchableOpacity } from "react-native";
+import { Image, TouchableOpacity, ScrollView} from "react-native";
 
 import { Text, View } from "../components/Themed";
 import MenuIcon from "../components/MenuIcon";
@@ -17,40 +17,60 @@ import {
 import { ClientStyle } from "../styles/clientStyle";
 
 export default function ClientsScreen() {
-  //Define navigation using the useNavigation hook
+  // Define navigation using the useNavigation hook.
   const navigation = useNavigation();
+
+  const [LoginSignOutText, setLoginSignOutText] = useState("Sign in");
+
+  const [isVisible, setIsVisible] = useState(true);
+
   
-  // Define state variables to hold profile and sign in/out text
+  // Define state variables to hold profile and sign in/out text.
   const [profileText, setProfileText] = useState(
     "Please log into your account"
   );
-  const [LoginSignOutText, setLoginSignOutText] = useState("Sign in");
 
-  // Set the headerLeft icon to the menu icon
+
+
+  // Set the headerLeft icon to the menu icon.
   useEffect(() => {
     navigation.setOptions({
       headerLeft: (props: StackHeaderLeftButtonProps) => <MenuIcon />,
     });
-    // Set profile and sign in/out text when screen is focused
-    navigation.addListener("focus", () => {
+      // Set profile and sign in/out text when screen is focused.
+      navigation.addListener("focus", () => {
       setProfileText(getUserNameTextForProfilePage());
       setLoginSignOutText(getLoginSignOutButtonText());
     });
+
+    if(getuserIsAuthenticated()) {
+      setIsVisible(true); 
+    } else {
+      setIsVisible(false); 
+    }
+    
+    
   });
 
-  // Define function for sign in/out button pressed
+  // Define function for login sign out button pressed.
   const authActions = () => {
     if (getuserIsAuthenticated()) {
       LoginSignOutButtonPressed();
       setProfileText("Please login to view account");
-      setLoginSignOutText("Sign in");
+      setLoginSignOutText("Login");
+      setIsVisible(false);
+
     } else {
       navigation.navigate("Login");
     }
+
   };
 
   return (
-    // Render the main content of the screen using a View component
+    /* Render the main content of the screen using a View component. 
+    This content is rendered in a scroll view, which allows for a dynamic menu that can be moved up and down.
+    */
+    <ScrollView style={ClientStyle.scrollView}>
     <View style={ClientStyle.content}>
       <View style={ClientStyle.topPageContent}>
         {/* Display Evoleon logo image */}
@@ -95,8 +115,19 @@ export default function ClientsScreen() {
             style={ClientStyle.arrow}
           />
         </TouchableOpacity>
+      
+      {/* This hides the delete button when the status chnages. */}
+      {isVisible && (
+        <TouchableOpacity style={ClientStyle.profileActionsCell}>
+          <Text style={ClientStyle.profileActionsText}>Delete Account</Text>
+          <Image
+            source={require("../assets/Arrow.png")}
+            style={ClientStyle.arrow}
+          />
+        </TouchableOpacity>
+      )}
         
-        {/* Display the sign in/out button with its text and icon */}
+        {/* Display the login or sign out button with its text and icon */}
         <TouchableOpacity
           style={ClientStyle.profileActionsCell}
           onPress={() => {
@@ -113,5 +144,6 @@ export default function ClientsScreen() {
         </TouchableOpacity>
       </View>
     </View>
+    </ScrollView>
   );
 }
