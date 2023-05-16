5c04ed8143f30bba45bf324cb5fdae27196bda8b
X0RA <annand.rodney@gmail.com>
2023-05-16 16:03:49 +1000

    Styled UserDetails page

--- screens/UpdateUserDetails.tsx
+++ screens/UpdateUserDetails.tsx
@@ -1,110 +1,103 @@
-//Import required modules
 import React, { useState, useEffect } from "react";
 import { View, Text, TextInput, TouchableOpacity } from "react-native";
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from "@react-navigation/native";
 import MenuIcon from "../components/MenuIcon";
 import { StackHeaderLeftButtonProps } from "@react-navigation/stack";
 import { UserDetailsPageStyle } from "../styles/updateUserDetails";
 import { ButtonStyle } from "../styles/buttonStyle";
 import { updateUserData } from "../web/firebase";
 
-//Declare an interface for user details
+
 interface UserDetails {
-  name: string;
-  email: string;
-  phone: string;
-  residentialAddress: string;
-  registrationNumber: string;
-  carType: string;
+    name: string;
+    email: string;
+    phone: string;
+    residentialAddress: string;
+    registrationNumber: string;
+    carType: string;
 }
 
-//React component for screen, uses useNavigation hook to get navigation object
 export default function UpdateUserDetailsScreen() {
-  const navigation = useNavigation();
+    const navigation = useNavigation();
 
-  //This hook helps to set options for screen's header
-  useEffect(() => {
-    navigation.setOptions({
-      headerLeft: (props: StackHeaderLeftButtonProps) => <MenuIcon />,
+    useEffect(() => {
+        navigation.setOptions({
+            headerLeft: (props: StackHeaderLeftButtonProps) => <MenuIcon />,
+        });
+    }, [navigation]);
+
+    const [userDetails, setUserDetails] = useState<UserDetails>({
+        name: "",
+        email: "",
+        phone: "",
+        residentialAddress: "",
+        registrationNumber: "",
+        carType: "",
     });
-  }, [navigation]);
 
-  //Initializing user details state with the useState hook
-  const [userDetails, setUserDetails] = useState<UserDetails>({
-    name: "",
-    email: "",
-    phone: "",
-    residentialAddress: "",
-    registrationNumber: "",
-    carType: "",
-  });
+    const handleUpdate = (key: keyof UserDetails, value: string) => {
+        setUserDetails((prevUserDetails) => ({
+            ...prevUserDetails,
+            [key]: value,
+        }));
+    };
 
-  //Function to handle update to user details
-  const handleUpdate = (key: keyof UserDetails, value: string) => {
-    setUserDetails((prevUserDetails) => ({
-      ...prevUserDetails,
-      [key]: value, //Update specified key with new value
-    }));
-  };
+    return (
+        <View style={UserDetailsPageStyle.container}>
+            <View style={UserDetailsPageStyle.profileContainer}>
+                          {/* avatar goes here */}
+            </View>
+            <View style={UserDetailsPageStyle.userDetailsForm}>
+                <Text style={UserDetailsPageStyle.userDetailsLabel}>Name:</Text>
+                <TextInput
+                    style={UserDetailsPageStyle.input}
+                    onChangeText={(text) => handleUpdate("name", text)}
+                    value={userDetails.name}
+                />
 
-  //UI rendering
-  return (
-    <View style={UserDetailsPageStyle.container}>
-      <View style={UserDetailsPageStyle.userDetailsForm}>
-        {/* Label and input field for name */}
-        <Text style={UserDetailsPageStyle.userDetailsLabel}>Name:</Text>
-        <TextInput
-          style={UserDetailsPageStyle.userDetailsInput}
-          onChangeText={(text) => handleUpdate("name", text)}
-          value={userDetails.name} // Bind input field value to corresponding state value
-        />
-         {/* Label and input field for email */}
-        <Text style={UserDetailsPageStyle.userDetailsLabel}>Email:</Text>
-        <TextInput
-          style={UserDetailsPageStyle.userDetailsInput}
-          onChangeText={(text) => handleUpdate("email", text)}
-          value={userDetails.email}
-        />
-        {/* Label and input field for phone */}
-        <Text style={UserDetailsPageStyle.userDetailsLabel}>Phone:</Text>
-        <TextInput
-          style={UserDetailsPageStyle.userDetailsInput}
-          onChangeText={(text) => handleUpdate("phone", text)}
-          value={userDetails.phone}
-        />
-        {/* Label and input field for residential address */}
-        <Text style={UserDetailsPageStyle.userDetailsLabel}>
-          Residential Address:
-        </Text>
-        <TextInput
-          style={UserDetailsPageStyle.userDetailsInput}
-          onChangeText={(text) => handleUpdate("residentialAddress", text)}
-          value={userDetails.residentialAddress}
-        />
-        {/* Label and input field for registration number */}
-        <Text style={UserDetailsPageStyle.userDetailsLabel}>
-          Registration Number:
-        </Text>
-        <TextInput
-          style={UserDetailsPageStyle.userDetailsInput}
-          onChangeText={(text) => handleUpdate("registrationNumber", text)}
-          value={userDetails.registrationNumber}
-        />
-        {/* Label and input field for car type */}
-        <Text style={UserDetailsPageStyle.userDetailsLabel}>Car Type:</Text>
-        <TextInput
-          style={UserDetailsPageStyle.userDetailsInput}
-          onChangeText={(text) => handleUpdate("carType", text)}
-          value={userDetails.carType}
-        />
-      </View>
-      {/* Button to save user details */}
-      <TouchableOpacity
-        style={ButtonStyle.Button}
-        onPress={updateUserData}// Handle button press event using function from firebase JS file
-      >
-        <Text style={ButtonStyle.Text}>Save</Text>
-      </TouchableOpacity>
-    </View>
-);
+                <Text style={UserDetailsPageStyle.userDetailsLabel}>Email:</Text>
+                <TextInput
+                    style={UserDetailsPageStyle.input}
+                    onChangeText={(text) => handleUpdate("email", text)}
+                    value={userDetails.email}
+                />
+
+                <Text style={UserDetailsPageStyle.userDetailsLabel}>Phone:</Text>
+                <TextInput
+                    style={UserDetailsPageStyle.input}
+                    onChangeText={(text) => handleUpdate("phone", text)}
+                    value={userDetails.phone}
+                />
+
+                <Text style={UserDetailsPageStyle.userDetailsLabel}>Residential Address:</Text>
+                <TextInput
+                    style={UserDetailsPageStyle.input}
+                    onChangeText={(text) => handleUpdate("residentialAddress", text)}
+                    value={userDetails.residentialAddress}
+                />
+
+                <Text style={UserDetailsPageStyle.userDetailsLabel}>Registration Number:</Text>
+                <TextInput
+                    style={UserDetailsPageStyle.input}
+                    onChangeText={(text) => handleUpdate("registrationNumber", text)}
+                    value={userDetails.registrationNumber}
+                />
+
+                <Text style={UserDetailsPageStyle.userDetailsLabel}>Car Type:</Text>
+                <TextInput
+                    style={UserDetailsPageStyle.input}
+                    onChangeText={(text) => handleUpdate("carType", text)}
+                    value={userDetails.carType}
+                />
+            </View>
+
+            {/* Button to save user details */}
+            <TouchableOpacity
+                style={ButtonStyle.Button}
+                onPress={updateUserData}// Handle button press event using function from firebase JS file
+            >
+                <Text style={ButtonStyle.Text}>Save</Text>
+            </TouchableOpacity>
+        </View>
+    );
 }
--- styles/updateUserDetails.ts
+++ styles/updateUserDetails.ts
@@ -1,48 +1,47 @@
 import { StyleSheet } from "react-native";
 
 export const UserDetailsPageStyle = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  userDetailsForm: {
-    marginTop: 50,
-    marginBottom: 50,
-    width: "80%",
-  },
-  userDetailsLabel: {
-    fontWeight: "bold",
-    fontSize: 16,
-    marginBottom: 5,
-  },
-  userDetailsInputContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    marginBottom: 20,
-  },
-  userDetailsInput: {
-    flex: 1,
-    height: 40,
-    borderWidth: 1,
-    borderColor: "gray",
-    borderRadius: 5,
-    padding: 10,
-  },
-  userDetailsButton: {
-    backgroundColor: "#2196F3",
-    padding: 10,
-    borderRadius: 5,
-  },
-  userDetailsButtonText: {
-    color: "#fff",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  userDetailsText: {
-    fontSize: 16,
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+    },
+    profileContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 20,
+    },
+    profileImage: {
+        height: 100,
+        width: 100,
+        borderRadius: 50,
+    },
+    userDetailsForm: {
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+    },
+    userDetailsLabel: {
+        fontSize: 16,
+        fontWeight: "bold",
+        marginBottom: 5,
+    },
+    input: {
+        height: 40,
+        borderColor: "gray",
+        borderWidth: 1,
+        borderRadius: 5,
+        paddingHorizontal: 10,
+        marginBottom: 10,
+    },
+    button: {
+        backgroundColor: "#4CAF50",
+        padding: 10,
+        marginHorizontal: 20,
+        marginBottom: 20,
+        borderRadius: 5,
+        alignItems: "center",
+    },
+    buttonText: {
+        color: "white",
+        fontSize: 16,
+        fontWeight: "bold",
+    },
 });
