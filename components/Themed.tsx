7d8ad55d86cd5aa08211d6b6c020d83e0a531d37
KNSHQ <kanishkrajvanshi@gmail.com>
2023-05-19 13:27:12 +1000

    Refactored Themed.tsx with following modifications: 1. Added destructuring props in the Feather component function argument to make it a bit cleaner.

--- components/Themed.tsx
+++ components/Themed.tsx
@@ -5,6 +5,7 @@ import { Feather as DefaultFeather } from '@expo/vector-icons';
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
 
+// A hook to select a color based on the theme and priority of colors passed in props
 export function useThemeColor(
   props: { light?: string; dark?: string },
   colorName: keyof typeof Colors.light & keyof typeof Colors.dark
@@ -30,10 +31,12 @@ type DefaultIconProps = {
   style: object | [object];
 }
 
+// Types for text, view, and icon props
 export type TextProps = ThemeProps & DefaultText['props'];
 export type ViewProps = ThemeProps & DefaultView['props'];
 export type IconProps = ThemeProps & DefaultIconProps;
 
+// Themed Text component
 export function Text(props: TextProps) {
   const { style, lightColor, darkColor, ...otherProps } = props;
   const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
@@ -41,6 +44,7 @@ export function Text(props: TextProps) {
   return <DefaultText style={[{ color }, style]} {...otherProps} />;
 }
 
+// Themed View component
 export function View(props: ViewProps) {
   const { style, lightColor, darkColor, ...otherProps } = props;
   const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
@@ -48,8 +52,8 @@ export function View(props: ViewProps) {
   return <DefaultView style={[{ backgroundColor }, style]} {...otherProps} />;
 }
 
-export function Feather(props: IconProps) {
-  const { lightColor, darkColor, name, size, style } = props;
+// Themed Feather Icon component
+export function Feather({ lightColor, darkColor, name, size, style }: IconProps) {
   const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
 
   return <DefaultFeather name={name} size={size} color={color} style={style}/>
