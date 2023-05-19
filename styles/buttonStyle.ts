be579eb3205a70c39961a7ee49d2d06c2dd0a7f6
KNSHQ <kanishkrajvanshi@gmail.com>
2023-05-19 13:50:59 +1000

    Refactored buttonStyle.ts with following modifications: 1. Added comments for better understanding and readability.

--- styles/buttonStyle.ts
+++ styles/buttonStyle.ts
@@ -1,39 +1,44 @@
-import {StyleSheet} from "react-native";
+import { StyleSheet } from 'react-native';
 
+// StyleSheet for the button components
 export const ButtonStyle = StyleSheet.create({
-    Button: {
-        alignItems: "center",
-        justifyContent: "center",
-        paddingVertical: 12,
-        borderRadius: 50,
-        backgroundColor: "#294E4B",
-        width: "auto",
-        minWidth: 300,
-        marginBottom: 10,
-    },
+  // Standard button style
+  Button: {
+    alignItems: 'center', // Align the content of the button in the center (horizontal alignment)
+    justifyContent: 'center', // Center the content of the button vertically
+    paddingVertical: 12, // Add vertical padding
+    borderRadius: 50, // Round the corners
+    backgroundColor: '#294E4B', // Set the background color
+    width: 'auto', // Automatically determine the width based on the content and padding
+    minWidth: 300, // But also ensure that it has a minimum width
+    marginBottom: 10, // Add a margin at the bottom
+  },
 
-    BackButton: {
-        alignItems: "center",
-        justifyContent: "center",
-        paddingVertical: 12,
-        borderRadius: 50,
-        backgroundColor: "#294E4B",
-        width: "auto",
-        minWidth: 300,
-        marginBottom: 10,
-        marginTop: 30,
-    },
+  // Style for the back button
+  BackButton: {
+    alignItems: 'center', // Align the content of the button in the center (horizontal alignment)
+    justifyContent: 'center', // Center the content of the button vertically
+    paddingVertical: 12, // Add vertical padding
+    borderRadius: 50, // Round the corners
+    backgroundColor: '#294E4B', // Set the background color
+    width: 'auto', // Automatically determine the width based on the content and padding
+    minWidth: 300, // But also ensure that it has a minimum width
+    marginBottom: 10, // Add a margin at the bottom
+    marginTop: 30, // Add a margin at the top
+  },
 
-    Text: {
-        fontSize: 16,
-        lineHeight: 21,
-        fontWeight: "bold",
-        letterSpacing: 0.25,
-        color: "white",
-    },
+  // Style for the text in the button
+  Text: {
+    fontSize: 16, // Set the font size
+    lineHeight: 21, // Set the line height
+    fontWeight: 'bold', // Make the font bold
+    letterSpacing: 0.25, // Increase the space between each letter
+    color: 'white', // Set the text color
+  },
 
-    LoginLink: {
-        color: "#294E4B",
-        fontWeight: "bold",
-    },
+  // Style for the login link
+  LoginLink: {
+    color: '#294E4B', // Set the link color
+    fontWeight: 'bold', // Make the link bold
+  },
 });
