2db96d826de52f4454c02748276daf499341d16c
KNSHQ <kanishkrajvanshi@gmail.com>
2023-05-19 14:11:40 +1000

    Refactored logoStyle.ts with following modifications: 1. Added comments for better understanding and readability.

--- styles/logoStyle.ts
+++ styles/logoStyle.ts
@@ -1,9 +1,12 @@
-import { StyleSheet, } from 'react-native';
+// Importing StyleSheet from react-native
+import { StyleSheet } from 'react-native';
 
+// Creating and exporting styles
 export default StyleSheet.create({
+    // Style for the front page logo
     frontPageLogo: {
-        width: 200,
-        height: 200,
-        marginBottom: 125
+        width: 200, // setting the width of the logo
+        height: 200, // setting the height of the logo
+        marginBottom: 125, // setting the bottom margin of the logo
     }
 });
