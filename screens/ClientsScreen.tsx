e20bb081e4031d4f8aa4739b2155f012d7ab923c
JoelM-D <jmurphydyer@deakin.edu.au>
2023-09-13 20:58:56 +1000

    Update ClientsScreen.tsx

--- screens/ClientsScreen.tsx
+++ screens/ClientsScreen.tsx
@@ -69,7 +69,7 @@ export default function ClientsScreen() {
           action={() => {
             navigation.navigate('Authenticate');
           }}
-          text="Login"
+          text="Login/Sign up"
           iconName="login"
         />
       )}
@@ -97,60 +97,81 @@ export default function ClientsScreen() {
 
       {getUserAuthStatus().Status && (
         <ListButton
-          action={() => {
-            navigation.navigate('Authenticate');
-          }}
-          text="Delete Account"
-          action={() => {
-            Alert.alert(
-              'Deletion request.',
-              'Are you sure you want to delete your account?',
-              [
-                {
-                  text: 'Cancel',
-                  onPress: () => {}, // Empty onPress function to close the alert
-                  style: 'cancel',
-                },
-                {
-                  text: 'Agree',
-                  onPress: async () => {
-                    Alert.alert(
-                      'This action cannot be undone.',
-                      'All user data will be removed from the application after this request.',
-                      [
-                        {
-                          text: 'Cancel',
-                          onPress: () => {}, // Empty onPress function to close the alert
-                          style: 'cancel',
-                        },
-                        {
-                          text: 'Understood',
-                          onPress: async () => {
+        action={() => {
+          Alert.alert(
+            'Deletion request.',
+            'Are you sure you want to delete your account?',
+            [
+              {
+                text: 'Cancel',
+                onPress: () => {}, // Empty onPress function to close the alert
+                style: 'cancel',
+              },
+              {
+                text: 'Agree',
+                onPress: async () => {
+                  Alert.alert(
+                    'This action cannot be undone.',
+                    'All user data will be removed from the application after this request.',
+                    [
+                      {
+                        text: 'Cancel',
+                        onPress: () => {}, // Empty onPress function to close the alert
+                        style: 'cancel',
+                      },
+                      {
+                        text: 'Understood',
+                        onPress: async () => {
+                          try {
+                            // Perform the asynchronous deletion logic here
                             await userDeleteAccount();
+
+                            // Update the user interface after successful deletion
                             setLoginSignOutText(getUserAuthStatus().Text);
                             setProfileText(UnauthProfileText);
-                          },
+
+                            // Show a success alert to the user
+                            Alert.alert(
+                              'Success',
+                              'Your account has been successfully deleted.',
+                              [
+                                {
+                                  text: 'OK',
+                                  onPress: () => {
+                                    // Navigate to the AuthenticateScreen
+                                    navigation.navigate('Authenticate');
+                                  },
+                                },
+                              ]
+                            );
+                          } catch (error) {
+                            // Handle any errors that occur during account deletion
+                            console.error('Error deleting account:', error);
+                          }
                         },
-                      ],
-                      { cancelable: false }
-                    );
-                  },
+                      },
+                    ],
+                    { cancelable: false }
+                  );
                 },
-              ],
-              { cancelable: false }
-            );
-          }}
-          iconName="remove-user"
-        />
-      )}
-        <ListButton
-          action={() => {
-            navigation.navigate('About');
-          }}
-          text="About"
-          iconName="info"
-        />
-      </ScrollView>
-    </View>
-  );
+              },
+            ],
+            { cancelable: false }
+          );
+        }}
+        text="Delete Account"
+        iconName="remove-user"
+      />
+    )}
+
+    <ListButton
+      action={() => {
+        navigation.navigate('About');
+      }}
+      text="About"
+      iconName="info"
+    />
+  </ScrollView>
+</View>
+);
 }
